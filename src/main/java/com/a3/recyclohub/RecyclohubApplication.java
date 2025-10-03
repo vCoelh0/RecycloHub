27725534ca2a17dfc39449c68abbe38f8c91c094
vCoelh0 <coelhocomercial2k@gmail.com>
2025-10-02 23:26:14 -0300

    upload inicial

---
+++ src/main/java/com/a3/recyclohub/RecyclohubApplication.java
@@ -0,0 +1,13 @@
+package com.a3.recyclohub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecyclohubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecyclohubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/a3/recyclohub/RecyclohubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.a3.recyclohub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecyclohubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
