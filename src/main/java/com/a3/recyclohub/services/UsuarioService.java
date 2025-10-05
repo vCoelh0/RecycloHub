5c3573b0f12ad41e5c6d3f079de4caaf47bf8cda
vCoelh0 <coelhocomercial2k@gmail.com>
2025-10-05 16:41:48 -0300

    [REQ-F-03]: Adicionado UsuarioService | Metodo Cadastrar Usuario

---
+++ src/main/java/com/a3/recyclohub/services/UsuarioService.java
@@ -0,0 +1,36 @@
+package com.a3.recyclohub.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.a3.recyclohub.dto.UsuarioDTO;
+import com.a3.recyclohub.entites.Usuario;
+import com.a3.recyclohub.entites.perfilUsuario;
+import com.a3.recyclohub.entites.repositories.UsuarioRespository;
+
+@Service
+public class UsuarioService {
+
+	@Autowired
+	private UsuarioRespository repository;
+	
+	public UsuarioDTO cadastrar(UsuarioDTO dto) {
+		
+		Usuario usuario = new Usuario();
+		
+		usuario.setNome(dto.getNome());
+		usuario.setSobrenome(dto.getSobrenome());
+		usuario.setEmail(dto.getEmail());
+		usuario.setDataNascimento(dto.getDataNascimento());
+		usuario.setSenha(dto.getSenha());
+		usuario.setPerfil(perfilUsuario.USUARIO);
+		
+		repository.save(usuario);
+		return new UsuarioDTO(usuario);
+		
+		
+		
+	}
+	
+	
+}
