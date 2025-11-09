0bc0dfbb22e1d814810305327f86ddcba5a6e04e
vCoelh0 <coelhocomercial2k@gmail.com>
2025-11-09 19:02:56 -0300

    config: Atualizado CORS

--- src/main/java/com/a3/recyclohub/configs/WebConfig.java
+++ src/main/java/com/a3/recyclohub/configs/WebConfig.java
@@ -11,7 +11,10 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebConfig implements WebMvcConfigurer{
 	
 	public void addCorsMappings(CorsRegistry registry) {
-		registry.addMapping("/**");
+		registry.addMapping("/**")
+        .allowedOrigins("http://127.0.0.1:5500", "http://localhost:5500", "http://127.0.0.1:5501", "http://localhost:5501" ) 
+        .allowedMethods("GET", "POST", "PUT", "DELETE")
+        .allowCredentials(true);
 	}
 	
 
