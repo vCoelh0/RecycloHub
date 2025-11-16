fbf44a22132c8824a76bb90e5f545168f39e5691
vCoelh0 <coelhocomercial2k@gmail.com>
2025-11-16 13:10:13 -0300

    config: Correção CORS

--- src/main/java/com/a3/recyclohub/configs/WebConfig.java
+++ src/main/java/com/a3/recyclohub/configs/WebConfig.java
@@ -14,6 +14,8 @@ public class WebConfig implements WebMvcConfigurer{
 		registry.addMapping("/**")
         .allowedOrigins("http://127.0.0.1:5500", "http://localhost:5500", "http://127.0.0.1:5501", "http://localhost:5501" ) 
         .allowedMethods("GET", "POST", "PUT", "DELETE")
+        .allowedHeaders("*")
+        .exposedHeaders("*")
         .allowCredentials(true);
 	}
 	
